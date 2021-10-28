8411d7c847479a2faea43eab7964101bafc2261e
Andrei Parkhomenko <ataflava@gmail.com>
2021-10-28 20:34:52 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {FC} from 'react';
+import QuestionArea from './components/question-area'
+import AnswerArea from './components/answer-area'
 
-function App() {
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-con'>
+      <QuestionArea question={'random text'}/>
+      <AnswerArea choiceAnswer={()=> console.log('hello')} answers={ {variable1: 'rendom text', variable2: 'random text', variable3: 'random text', variable4: 'random text'} }/>
     </div>
   );
 }
